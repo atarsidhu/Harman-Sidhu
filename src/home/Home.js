95130af3e1f9aeb0b5b0d5a24bdd92066c17b3fa
Atar Sidhu <a.sidhu14@hotmail.com>
2021-03-25 11:39:25 -0700

    Home carousel

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,11 +1,61 @@
 import React from "react";
 import "./Home.css";
+import { CarouselProvider, Slider, Slide, DotGroup } from "pure-react-carousel";
+import "pure-react-carousel/dist/react-carousel.es.css";
 
 function Home() {
   return (
     <div className="home">
       <div className="home__image"></div>
-      <img src="/images/house-3.jpg" alt="home-image" className="homeImage" />
+      <div className="carousel">
+        <CarouselProvider
+          naturalSlideHeight={100}
+          naturalSlideWidth={100}
+          totalSlides={5}
+          isPlaying={true}
+          infinite={true}
+        >
+          <Slider>
+            <Slide index={0}>
+              <img
+                src="/images/house-3.jpg"
+                alt="home-image"
+                className="homeImage"
+              />
+            </Slide>
+            <Slide index={1}>
+              <img
+                src="/images/backyard.jpg"
+                alt="home-image"
+                className="homeImage"
+              />
+            </Slide>
+            <Slide index={2}>
+              <img
+                src="/images/house-2.jpg"
+                alt="home-image"
+                className="homeImage"
+              />
+            </Slide>
+            <Slide index={3}>
+              <img
+                src="/images/interior.jpg"
+                alt="home-image"
+                className="homeImage"
+              />
+            </Slide>
+            <Slide index={4}>
+              <img
+                src="/images/house-1.jpg"
+                alt="home-image"
+                className="homeImage"
+              />
+            </Slide>
+          </Slider>
+          <DotGroup className="dots" />
+        </CarouselProvider>
+      </div>
+
       <div className="home__content">
         <h1 className="name">Harman S. Sidhu</h1>
         <p className="title">Personal Real Estate Corporation</p>
