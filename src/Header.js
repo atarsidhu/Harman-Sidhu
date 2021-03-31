9955a22e9c3961c40559d472be383f9d66804dfa
Atar Sidhu <a.sidhu14@hotmail.com>
2021-03-31 16:15:49 -0700

    Header style changed

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import "./Header.css";
 
 function Header() {
   const [sticky, setSticky] = useState(false);
+  let location = useLocation();
 
   const handleScroll = () => {
     const offset = window.scrollY;
@@ -24,29 +25,67 @@ function Header() {
   }, []);
 
   return (
-    <div className={sticky ? "header sticky-background" : "header"}>
+    <div
+      className={
+        location.pathname !== "/"
+          ? sticky
+            ? "header headerNotHomeSticky"
+            : "header headerNotHome"
+          : sticky
+          ? "header sticky-background"
+          : "header"
+      }
+    >
       <div className="header__logo">
         <h3 className="logo">Harman Sidhu</h3>
       </div>
       <div className="header__nav">
-        <Link to="/" className="link">
+        <NavLink to="/" className="link">
           <p
-            className={sticky ? "header__option stick-text" : "header__option"}
+            className={
+              location.pathname !== "/"
+                ? "header__option stick-text"
+                : sticky
+                ? "header__option stick-text"
+                : "header__option"
+            }
           >
             Home
           </p>
-        </Link>
-        <Link to="/listings" className="link">
+        </NavLink>
+        <NavLink to="/listings" className="link" activeClassName="selected">
           <p
-            className={sticky ? "header__option stick-text" : "header__option"}
+            className={
+              location.pathname !== "/"
+                ? "header__option stick-text"
+                : sticky
+                ? "header__option stick-text"
+                : "header__option"
+            }
           >
             Listings
           </p>
-        </Link>
-        <p className={sticky ? "header__option stick-text" : "header__option"}>
+        </NavLink>
+        <p
+          className={
+            location.pathname !== "/"
+              ? "header__option stick-text"
+              : sticky
+              ? "header__option stick-text"
+              : "header__option"
+          }
+        >
           About
         </p>
-        <p className={sticky ? "header__option stick-text" : "header__option"}>
+        <p
+          className={
+            location.pathname !== "/"
+              ? "header__option stick-text"
+              : sticky
+              ? "header__option stick-text"
+              : "header__option"
+          }
+        >
           Contact
         </p>
       </div>
