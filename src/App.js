c02fdee4d36099d069cd7dee0a49b5d53b95c6e7
Atar Sidhu <a.sidhu14@hotmail.com>
2021-04-08 11:40:46 -0700

    Added Footer component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./Header";
 import Home from "./home/Home";
 import Listings from "./listings/Listings";
+import Footer from "./footer/Footer";
 
 function App() {
   return (
     <Router>
-      <div className="App">
+      <div className="app">
         <Header />
         <Switch>
           <Route path="/listings">
@@ -17,6 +18,7 @@ function App() {
             <Home />
           </Route>
         </Switch>
+        <Footer />
       </div>
     </Router>
   );
