550853a99ec1c82b0c1ba6a7f261ad4f811326b2
Atar Sidhu <a.sidhu14@hotmail.com>
2021-04-26 18:56:21 -0700

    Added logo and navigation

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -6,10 +6,31 @@ import EmailIcon from "@material-ui/icons/Email";
 function Footer() {
   return (
     <div className="footer">
-      <h3>Logo</h3>
-      <h3>Something</h3>
+      {/* <h3>Logo</h3> */}
+      <div className="footer__logo">
+        <img
+          src="/images/HarmanSidhuLogo4GoldWhite.png"
+          alt=""
+          style={{ height: "125px" }}
+        />
+      </div>
+      <div className="footer__links">
+        <h4 className="mb-4">Navigate</h4>
+        <a href="/" className="footer-link">
+          Home
+        </a>
+        <a href="/listings" className="footer-link">
+          Listings
+        </a>
+        <a href="/about" className="footer-link">
+          About
+        </a>
+        <a href="/contact" className="footer-link">
+          Contact
+        </a>
+      </div>
       <div className="footer__contact">
-        <h3>Contact</h3>
+        <h4 className="mb-4">Contact</h4>
         <div className="contact__office">
           <img
             src="/images/remax-blueprint.png"
@@ -24,7 +45,7 @@ function Footer() {
         <div className="contact__personal">
           <div className="personal__phone">
             <PhoneIcon className="footer__icon" />
-            <p className="contact-content pl-3">604-593-6188</p>
+            <p className="contact-content pl-3">604-600-5854</p>
           </div>
           <div className="personal__email">
             <EmailIcon className="footer__icon" />
