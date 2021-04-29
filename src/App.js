472e35ab5c6338341d61577351f030eb29a24614
Atar Sidhu <a.sidhu14@hotmail.com>
2021-04-28 20:37:04 -0700

    Added Contact and About pages to routes

--- src/App.js
+++ src/App.js
@@ -4,13 +4,27 @@ import Header from "./Header";
 import Home from "./home/Home";
 import Listings from "./listings/Listings";
 import Footer from "./footer/Footer";
+import About from "./about/About";
+import Contact from "./contact/Contact";
+import ScrollToTop from "./ScrollToTop";
+import DetailedListing from "./listings/DetailedListing";
 
 function App() {
   return (
     <Router>
       <div className="app">
         <Header />
+        <ScrollToTop />
         <Switch>
+          <Route path="/listing/:id" exact component={DetailedListing}>
+            {/* <DetailedListing /> */}
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
           <Route path="/listings">
             <Listings />
           </Route>
